1dad39d82622634621647443c6a286438ee12895
AlshimaaGamalAlsaied <al-sh.gamal@outlook.com>
2020-02-25 21:49:43 +0300

    update

--- CV404Filters.py
+++ CV404Filters.py
@@ -3,8 +3,9 @@ from os.path import isfile , join
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from scipy import ndimage, signal
-from skimage import filters
+from PIL import Image  
+from scipy import ndimage, signal,misc
+
 
 def rgb2gray(rgb_image):
     return np.dot(rgb_image[...,:3], [0.299, 0.587, 0.114])
@@ -16,22 +17,16 @@ def multi_view( images ):
         if row % 2==0:
             ax1 = fig.add_subplot(images_count , 2, row+1)
             ax1.title.set_text('Before noise')
-        #ax1.imshow( images[ row ] )
             ax1.imshow(images[ row ], cmap=plt.get_cmap('gray')) # default is viridis 
             
         else:
             ax1 = fig.add_subplot(images_count , 2, row+1)
             ax1.title.set_text('After noise')
-        #ax1.imshow( images[ row ] )
-            ax1.imshow(images[ row ], cmap=plt.get_cmap('gray')) # default is viridis 
+            ax1.imshow(images[ row ], cmap=plt.get_cmap('gray')) 
+           
             
 def gaussian_noise( mu, sigma, im_size ):
     randGaussian=np.random.normal( mu, sigma, im_size) #np.random.normal Gaussian noise
-    #print(randUniform) 
-    #print(np.mean(randUniform))
-    #print(np.std(randUniform))   
-    #print('\n')
-    
     return randGaussian
 
 def im_gaussian_noise(mu, sigma, im):
@@ -39,28 +34,24 @@ def im_gaussian_noise(mu, sigma, im):
     img_w_g_noise = im + g_noise
     return img_w_g_noise
 
-def Uniform(img):
-    uniform=np.random.random(img.shape)
-    return uniform            
         
+def Random_Uniform(img,percent):
+    img_noisy=np.zeros(img.shape)
+    uniform = np.random.random(img.shape) 
+    cleanPixels_ind=uniform > percent
+    noise = (uniform <= (percent)); 
+    img_noisy[cleanPixels_ind]=img[cleanPixels_ind]
+    img_noisy[noise] = 0.7
+    return img_noisy
 
 def salt_pepper_noise(img,percent):
     img_noisy=np.zeros(img.shape)
     salt_pepper = np.random.random(img.shape) # Uniform distribution
-    
-    #print(img.size)
-    #print(salt_pepper.size)
-    #print((salt_pepper > percent)*1)
-    #print(np.sum((salt_pepper > percent)*1)/img.size) #percent clean
     cleanPixels_ind=salt_pepper > percent
-    NoisePixels_ind=salt_pepper <= percent
+    #NoisePixels_ind=salt_pepper <= percent
     pepper = (salt_pepper <= (0.5* percent)); # pepper < half percent
     
     salt = ((salt_pepper <= percent) & (salt_pepper > 0.5* percent)); 
-    
-    #print(np.sum((salt)*1)/img.size)
-    #print(cleanPixels_ind.shape)
-    #print(img_noisy.shape)
     img_noisy[cleanPixels_ind]=img[cleanPixels_ind]
     img_noisy[pepper] = 0
     img_noisy[salt] = 1
@@ -75,14 +66,15 @@ result=[im_gaussian_noise(0, 0.3, img ) for img in gray_images]
 
 print(len(result))
 print(len(result[0][0]))
-
 # Gaussian
 [multi_view(viewNoise) for viewNoise in (list( zip( gray_images , result )))]
 
+
 ## Uniform
-uniform_noise = [Uniform(grimages) for grimages in gray_images]
+uniform_noise = [Random_Uniform (grimages,0.3) for grimages in gray_images]
 [multi_view(viewNoise) for viewNoise in (list( zip( gray_images , uniform_noise )))]
 
 # Salt & Pepper
 salt_pepper_noise_imgs = [salt_pepper_noise(grimages,0.5) for grimages in gray_images]
-[multi_view(viewNoise) for viewNoise in (list( zip( gray_images , salt_pepper_noise_imgs )))]
+[multi_view(viewNoise) for viewNoise in (list( zip( gray_images , salt_pepper_noise_imgs )))]   
+    
